377f72f98f58c0cdad409380eb12152f8f915d58
Your Name <your.email@example.com>
2026-04-10 15:51:14 -0300

    commit inicial, projeto spring boot GASPO

---
+++ src/main/java/com/gaspo/api/GaspoApplication.java
@@ -0,0 +1,13 @@
+package com.gaspo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaspoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaspoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaspo/api/GaspoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaspo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaspoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
