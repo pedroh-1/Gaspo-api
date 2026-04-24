277bc0c067f4c093c2bdd20daa33c10cf90a22f2
pedroh-1 <po140333@gmail.com>
2026-04-24 08:57:39 -0300

    commit ConsultaModel

--- src/main/java/com/gaspo/api/service/ConsultaService.java
+++ src/main/java/com/gaspo/api/service/ConsultaService.java
@@ -1,4 +1,23 @@
 package com.gaspo.api.service;
 
+import com.gaspo.api.model.ConsultaModel;
+import com.gaspo.api.repository.ConsultaRepository;
+import com.gaspo.api.repository.ProfissionalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ConsultaService {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @Autowired
+    private ProfissionalRepository profissionalRepository;
+
+    public ConsultaModel realizarAgendamento(ConsultaModel consulta) {
+
+        //validação do profissional
+        if (!profissionalRepository.existsById(consulta.getProfissional().getId())) {
+            throw new RuntimeException("Profissional não foi encontrado no sistema!");
+        }
+    }
 }
