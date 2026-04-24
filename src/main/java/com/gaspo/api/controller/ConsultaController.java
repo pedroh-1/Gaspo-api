3fac9064cd5658d3a8d1bcea649bca56f38dc77d
pedroh-1 <po140333@gmail.com>
2026-04-24 10:10:09 -0300

    atualização das classes de consulta

--- src/main/java/com/gaspo/api/controller/ConsultaController.java
+++ src/main/java/com/gaspo/api/controller/ConsultaController.java
@@ -1,4 +1,38 @@
 package com.gaspo.api.controller;
 
+import com.gaspo.api.model.ConsultaModel;
+import com.gaspo.api.service.ConsultaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/consultas")
 public class ConsultaController {
+
+    @Autowired
+    private ConsultaService consultaService;
+
+    // cria um agendamento
+    @PostMapping
+    public ResponseEntity<ConsultaModel> agendar(@RequestBody ConsultaModel consulta){
+        ConsultaModel novaConsulta = consultaService.realizarAgendamento(consulta);
+        return ResponseEntity.ok(novaConsulta);
+    }
+
+    //Listar todas as consultas
+    @GetMapping
+    public ResponseEntity<List<ConsultaModel>> listarTodas() {
+        return ResponseEntity.ok(consultaService.listarTodas());
+    }
+
+    @PutMapping("/cancelar/{id}")
+    public ResponseEntity<String> cancelar(@PathVariable Long id){
+        consultaService.cancelarConsulta(id);
+        return ResponseEntity.ok("Consulta " + id + " cancelada om sucesso");
+    }
+
+
 }
