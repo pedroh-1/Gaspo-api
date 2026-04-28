7076918038ed3c926e4f20c957a44ae4b135c8bb
2024101202010026 <leticiainaciopvaz@gmail.com>
2026-04-28 16:27:14 -0300

    leticiaa

--- src/main/java/com/gaspo/api/service/AvisoService.java
+++ src/main/java/com/gaspo/api/service/AvisoService.java
@@ -1,4 +1,32 @@
 package com.gaspo.api.service;
 
+
+import com.gaspo.api.model.gaspo.AvisoModel;
+import com.gaspo.api.repository.gaspo.AvisoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
 public class AvisoService {
+
+    @Autowired
+    private AvisoRepository repository;
+
+    public List<AvisoModel> listarTodos() {
+        return repository.findAll();
+    }
+
+    public List<AvisoModel> listarAtivos() {
+        return repository.findByAtivoTrue();
+    }
+
+    public AvisoModel salvar(AvisoModel aviso) {
+        if (aviso.getDataPublicacao() == null) {
+            aviso.setDataPublicacao(LocalDateTime.now());
+        }
+        return repository.save(aviso);
+    }
 }
